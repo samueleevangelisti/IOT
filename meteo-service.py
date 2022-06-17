2a34839b6b5cbb2fbc29824fe185738f41d89189
Samuele Evangelisti <samueleevangelisti@yahoo.it>
2022-06-17 23:13:24 +0200

    Inizio implementazione service di download dei dati

--- meteo-service.py
+++ meteo-service.py
@@ -1,1 +1,16 @@
+from datetime import datetime, timedelta
+from meteostat import Stations, Hourly
+from time import sleep
 
+
+
+latitude = 43.777734
+logitude = 12.941052
+
+while True:
+  end = datetime.now()
+  start = end - timedelta(hours = 1)
+
+  hourly = Hourly(Stations().nearby(latitude, logitude).fetch(1), start, end).fetch().iloc[:, :1]
+
+  sleep(3600)
