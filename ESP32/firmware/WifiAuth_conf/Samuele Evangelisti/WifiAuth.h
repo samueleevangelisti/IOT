d79f737c663b90933d77e49fb8834360b50f16a4
Samuele Evangelisti <samueleevangelisti@yahoo.it>
2022-06-24 20:43:29 +0200

    i

---
+++ ESP32/firmware/WifiAuth_conf/Samuele Evangelisti/WifiAuth.h
@@ -0,0 +1,7 @@
+#ifndef WIFIAUTH_H
+#define WIFIAUTH_H
+
+char* WIFI_SSID = "TIM-32867173"; // nome della rete wifi
+char* WIFI_PASSWORD = "rbRWGmRFlmsXN3GulwBXByaG"; // password della rete wifi
+
+#endif
