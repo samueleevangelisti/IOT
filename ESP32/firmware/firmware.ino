8109d299c0bdb94850088a2a0024a5ed8abc14bf
giovanniPi997 <giovannipietrucci27@gmail.com>
2022-06-27 00:32:55 +0200

    Implementazione protocollo CoAP

--- ESP32/firmware/firmware.ino
+++ ESP32/firmware/firmware.ino
@@ -8,15 +8,16 @@
 #include "MqttUtils.h"
 #include "Dht11Utils.h"
 #include "Mq2Utils.h"
-
+#include "CoAP.h"
 void setup() {
   Serial.println("SYS   -> [WAIT] initialization");
   pinMode(PIN_MQ2_DO, INPUT);
   Serial.begin(115200);
   wifi_connect();
-  mqtt_init();
-  mqtt_connect();
+  //mqtt_init();
+  //mqtt_connect();
   dht11_init();
+  coap_setup();
   Serial.println("SYS   -> [OK  ] initialization");
 }
 
@@ -25,6 +26,7 @@ void loop() {
   wifi_read();
   dht11_read();
   mq2_read();
-  mqtt_publish();
+  //mqtt_publish();
+  coap_process();
   delay(SAMPLE_FREQUENCY);
 }
