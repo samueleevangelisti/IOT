16d4396e8a45bbda38768d94d50de443ef19d764
Samuele Evangelisti <samueleevangelisti@yahoo.it>
2022-08-09 14:52:32 +0200

    .

--- Services/influx.py
+++ Services/influx.py
@@ -1,5 +1,6 @@
 from os import path
 import json
+from datetime import datetime, timedelta
 from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
 
@@ -11,6 +12,7 @@ class Influx:
         self.client = InfluxDBClient(url=self.config['url'], token=self.config['token'], org=self.config['organization'])
         self.write_api = self.client.write_api(write_options=SYNCHRONOUS)
         self.query_api = self.client.query_api()
+        self.delete_api = self.client.delete_api()
 
     def write_weather(self, point_dict):
         print('INFLUX -> [WAIT] write weather')
@@ -35,4 +37,12 @@ class Influx:
             print('INFLUX -> [OK  ] write forecasting prophet')
         except:
             print('INFLUX -> [ERR ] write forecasting prophet')
+
+    def delete_forecasting_arima_all(self):
+        print('INFLUX -> [WAIT] delete forecasting arima')
+        try:
+            delete_api.delete('1970-01-01T00:00:00Z', (datetime.now() + timedelta(days=365)).timestamp() * 1000000000, bucket_id=self.config['bucket']['forecasting']['arima'], org_id=self.config['organization'])
+            print('INFLUX -> [OK  ] delete forecasting arima')
+        except:
+            print('INFLUX -> [ERR ] delete forecasting arima')
         
